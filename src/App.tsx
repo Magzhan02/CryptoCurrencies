850af7a3ed33a594f1cadad109065883db5a4031
Magzhan <sunmorring@gmail.com>
2022-03-29 17:14:36 +0600

    added crypto api and edit table cell

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 
-import axios from 'axios'
+import axios from 'axios';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -26,13 +26,7 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
+function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
   return { name, calories, fat, carbs, protein };
 }
 
@@ -44,41 +38,63 @@ const rows = [
   createData('Gingerbread', 356, 16.0, 49, 3.9),
 ];
 
+type typeCoin = {
+  name: string;
+  fullname: string;
+  imageUrl: string;
+  price: number;
+  volume24hour: number;
+};
+
 function App() {
+  const [data, setData] = React.useState<typeCoin[]>([]);
+
+  React.useEffect(() => {
+    axios
+      .get('https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD')
+      .then(({ data }) => {
+        const coins: typeCoin[] = data.Data.map((coin: any) => {
+          const obj: typeCoin = {
+            name: coin.CoinInfo.Name,
+            fullname: coin.CoinInfo.FullName,
+            imageUrl: `https://www.cryptocompare.com/${coin.CoinInfo.ImageUrl}`,
+            price: coin.RAW.USD.PRICE.toFixed(5),
+            volume24hour: parseInt(coin.RAW.USD.VOLUME24HOUR),
+          };
+          return obj;
+        });
+        setData(coins);
+      });
+  }, []);
+
   return (
     <Container maxWidth="lg" sx={{ padding: 10 }}>
       <Grid container spacing={3}>
         <Grid item xs={8}>
-         
-            <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell></TableCell>
-                    <TableCell align="left">FullName</TableCell>
-                    <TableCell align="left">Name</TableCell>
-                    <TableCell align="left">Price</TableCell>
-                    <TableCell align="left">volume24hour</TableCell>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell></TableCell>
+                  <TableCell align="left">Name</TableCell>
+                  <TableCell align="left">FullName</TableCell>
+                  <TableCell align="left">Price</TableCell>
+                  <TableCell align="left">volume24hour</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data.map((coin) => (
+                  <TableRow key={coin.name}>
+                    <TableCell><img width={30} src={coin.imageUrl} alt='Coin icon'/></TableCell>
+                    <TableCell align="left">{coin.name}</TableCell>
+                    <TableCell align="left">{coin.fullname}</TableCell>
+                    <TableCell align="left">${coin.price}</TableCell>
+                    <TableCell align="left">${coin.volume24hour}</TableCell>
                   </TableRow>
-                </TableHead>
-                <TableBody>
-                  {rows.map((row) => (
-                    <TableRow
-                      key={row.name}
-                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                      <TableCell component="th" scope="row">
-                        {row.name}
-                      </TableCell>
-                      <TableCell align="left">{row.calories}</TableCell>
-                      <TableCell align="left">{row.fat}</TableCell>
-                      <TableCell align="left">{row.carbs}</TableCell>
-                      <TableCell align="left">{row.protein}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Grid>
 
         <Grid item xs={4}>
